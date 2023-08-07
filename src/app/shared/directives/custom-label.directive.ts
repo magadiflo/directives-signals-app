d88dad7bad7b96b7ec5a364e87720de3a7d81f08
Martín <magadiflo@gmail.com>
2023-08-07 11:28:31 -0500

    Cambiar mensaje de la etiqueta

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]'
@@ -7,6 +8,7 @@ export class CustomLabelDirective implements OnInit {
 
   private _htmlElement?: ElementRef<HTMLElement>;
   private _color: string = 'blue';
+  private _errors: ValidationErrors | null = null;
 
   constructor(private _el: ElementRef<HTMLElement>) {
     console.log(this._el);
@@ -16,16 +18,45 @@ export class CustomLabelDirective implements OnInit {
   @Input()
   public set color(value: string) {
     this._color = value;
-    this.setStyle();
+    this._setStyle();
+  }
+
+  @Input()
+  public set errors(value: ValidationErrors | null) {
+    this._errors = value;
+    this._setErrorMessage();
   }
 
   ngOnInit(): void {
-    this.setStyle();
+    this._setStyle();
   }
 
-  setStyle(): void {
+  private _setStyle(): void {
     if (!this._htmlElement) return;
     this._htmlElement!.nativeElement.style.color = this._color;
+  }
+
+  private _setErrorMessage(): void {
+    if (!this._htmlElement) return;
+    if (!this._errors) {
+      this._htmlElement.nativeElement.innerText = 'No hay errores';
+      return;
+    }
+
+    const errorKeys = Object.keys(this._errors);
+    if (errorKeys.includes('required')) {
+      this._htmlElement.nativeElement.innerText = 'Este campo es requerido';
+      return;
+    }
+    if (errorKeys.includes('minlength')) {
+      console.log(this._errors);
+      this._htmlElement.nativeElement.innerText = `Se requieren mínimo ${this._errors['minlength']['requiredLength']} caracteres`;
+      return;
+    }
+    if (errorKeys.includes('email')) {
+      this._htmlElement.nativeElement.innerText = 'El valor ingresado no tiene un formato de correo.';
+      return;
+    }
   }
 
 }
