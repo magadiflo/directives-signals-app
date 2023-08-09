f98e87d50df8c64fe68c0ff336b43d8443cc79e6
Martín <magadiflo@gmail.com>
2023-08-09 10:59:30 -0500

    Señales - Información del usuario

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
+
+import { UsersService } from '../../services/users.service';
+import { User } from '../../interfaes/user-request.interface';
+
 
 @Component({
   selector: 'app-user-info-page',
@@ -6,6 +10,29 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
+
+  private _userService = inject(UsersService);
+
+  public userId = signal(1);
+  public currentUser = signal<User | undefined>(undefined);
+  public fullName = computed(() => this.currentUser()?.first_name + " " + this.currentUser()?.last_name);
+  public userWasFound = signal(true);
+
+  ngOnInit(): void {
+    this.loadUser(this.userId());
+  }
+
+  loadUser(id: number): void {
+    if (id <= 0) return;
+
+    this.userId.set(id);
+    this.currentUser.set(undefined);
+
+    this._userService.getUserById(id)
+      .subscribe(user => {
+        this.currentUser.set(user);
+      });
+  }
 
 }
